853f2480e00573acfdf5c212b6d0f798765f3ab3
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-24 14:48:37 +0530

    [aeiou].py

---
+++ RE_CharClass/LowercaseVowel_Match.py
@@ -0,0 +1,45 @@
+"""
+[aeiou]	 --  Match any one lowercase vowel
+"""
+
+# Importing 're' Module
+import re
+
+string = 'When, nothing goes @Right --> \
+         Go, @Left <--'
+
+# Matching
+match = re.search(r'[aeiou]', string, re.I)
+if match:
+    print 'Match:', match.group()
+else:
+    print 'Try Something Else!'
+    
+# or
+
+# Matching
+match = re.search(r'[o]', string, re.I)
+if match:
+    print 'Match:', match.group()
+else:
+    print 'Try Something Else!'
+    
+# or
+
+# Matching
+match = re.search(r'[i]', string, re.I)
+if match:
+    print 'Match:', match.group()
+else:
+    print 'Try Something Else!'
+    
+# or
+
+string = 'I\'m into python.'
+
+# Matching
+match = re.search(r'[o]', string, re.I)
+if match:
+    print 'Match:', match.group()
+else:
+    print 'Try Something Else!'
